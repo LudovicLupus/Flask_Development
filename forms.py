62a76fb4500336aabd2886c09c60bfedc83daa89
Luis Lopez <luis.lopez@therigy.com>
2020-07-19 14:34:10 -0400

    Installed and imported email_validator to resolve bug, and created .gitignore

--- forms.py
+++ forms.py
@@ -6,13 +6,14 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo  # VALIDATOR
 # Say you want to make a registration form - create registration class
 class RegistrationForm(FlaskForm):  # Extend the FlaskForm class for form creation
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()] )
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')     # You need a submit button
 
+
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()] )     # Using email to log in
+    email = StringField('Email', validators=[DataRequired(), Email()])     # Using email to log in
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')     # You need a submit button
