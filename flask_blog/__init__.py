e6c4c119e74f6e0a1fb3449775038db170412860
Luis Lopez <luis.lopez@therigy.com>
2020-07-22 21:55:37 -0400

    Imported flask_bcrypt and add db functionality in register route to save user information (passwords are hashed with bcrypt)

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,10 +1,12 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b01a54d78abbef243757547790d122ea'   # Convert this to environment variable later...
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'     # Relative path to the current file
 db = SQLAlchemy(app)    # SQLAlchemy database instance (pass your app as an arg when creating)
+bcrypt = Bcrypt(app)
 
 from flask_blog import routes
--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -5,7 +5,7 @@ from flask import flash             # for flashing messages (modifications made 
 from flask import redirect          # redirect to another route url
 from flask_blog.forms import RegistrationForm, LoginForm   # Importing forms from forms.py
 from flask_blog.models import User, Post
-from flask_blog import app
+from flask_blog import app, db, bcrypt
 
 # The variable 'posts' simulates a database response (two posts shown here)
 # i.e. this is dummy data
@@ -24,9 +24,11 @@ posts = [
     }
 ]
 
+
 @app.route('/hello')
 def hello_world():
     return '<h1>Hello, world!</h1>'     # Renders valid HTML
+
 
 @app.route('/')         # Decorator used to map URLs to functions
 @app.route('/home')     # You can have multiple routes to the same page
@@ -34,18 +36,22 @@ def index():
     # return 'Index Page (this is my HOME page)'    # Non-template rendering
     return render_template('home.html')     # Searches for html file in 'templates' dir
 
+
 @app.route('/about')
 def about():
     # return '<h1>About page</h1>'
     return render_template('about.html', title='About')
 
+
 @app.route('/blog')
 def blog():
     return render_template('blog.html', posts=posts)
 
+
 @app.route('/tomato')
 def tomato():
     return render_template('tomatoes.html', title='Tomato page for tomato heads')
+
 
 ###################
 ### FORM ROUTES ###
@@ -55,9 +61,14 @@ def register():
     # Create an instance of your RegistrationForm to pass to a template
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('blog'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.commit()
+        flash(f'Your account has been created! You are now able to log in.', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
